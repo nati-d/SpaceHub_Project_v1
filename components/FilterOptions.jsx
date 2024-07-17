a4c591eb2aa542c64dbbd9e83006bb7ab17502d6
Nathnael Desalegn <nathnaeldes@gmail.com>
2024-07-17 13:59:04 +0300

    filter updated

--- components/FilterOptions.jsx
+++ components/FilterOptions.jsx
@@ -1,10 +1,12 @@
-"use client"
+"use client";
 import React from 'react';
 import { Grid, Checkbox, RangeSlider, Text } from '@mantine/core';
 
 const FilterOptions = ({
-  selectedCategories,
-  handleCategoryChange,
+  selectedPropertyTypes,
+  handlePropertyTypeChange,
+  selectedDurations,
+  handleDurationChange,
   rangeValue,
   setRangeValue,
   clearFilters,
@@ -15,17 +17,30 @@ const FilterOptions = ({
       <div className="space-y-3">
         <Checkbox.Group
           label=""
-          value={selectedCategories}
-          onChange={handleCategoryChange}
+          value={selectedPropertyTypes}
+          onChange={handlePropertyTypeChange}
           className="space-y-3 mt-3"
         >
-          <label className="bold-16">Category</label>
+          <label className="bold-16">Property Type</label>
           <div className="space-y-2 mt-3">
-            <Checkbox value="Clothing" label="Clothing" color="dark" />
-            <Checkbox value="Accessories" label="Accessories" color="dark" />
-            <Checkbox value="Footwear" label="Footwear" color="dark" />
-            <Checkbox value="Bags" label="Bags" color="dark" />
-            <Checkbox value="Electronics" label="Electronics" color="dark" />
+            <Checkbox value="Office" label="Office" color="dark" />
+            <Checkbox value="Workshop" label="Workshop" color="dark" />
+            <Checkbox value="Lab" label="Lab" color="dark" />
+            <Checkbox value="Dedicated Desk" label="Dedicated Desk" color="dark" />
+            <Checkbox value="Shared Desk" label="Shared Desk" color="dark" />
+          </div>
+        </Checkbox.Group>
+        <Checkbox.Group
+          label=""
+          value={selectedDurations}
+          onChange={handleDurationChange}
+          className="space-y-3 mt-3"
+        >
+          <label className="bold-16">Duration</label>
+          <div className="space-y-2 mt-3">
+            <Checkbox value="Daily" label="Daily" color="dark" />
+            <Checkbox value="Monthly" label="Monthly" color="dark" />
+            <Checkbox value="Annually" label="Annually" color="dark" />
           </div>
         </Checkbox.Group>
         <div className="mt-3 space-y-3">
@@ -41,6 +56,11 @@ const FilterOptions = ({
             value={rangeValue}
             onChange={setRangeValue}
             color="dark"
+            marks={[
+              { value: 0, label: 'Under 600 Br' },
+              { value: 600, label: '600 - 1500 Br' },
+              { value: 1500, label: '1500 - 2000 Br' },
+            ]}
           />
         </div>
       </div>
